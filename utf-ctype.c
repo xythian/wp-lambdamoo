8f59bc6665be001ce75f2265b3f63164942db7ff
Rob Leslie <rob@mars.org>
2008-07-28 15:31:26 -0700

    Don't consider surrogate code points to be printable
    This commit alters my_is_printable() to reject surrogate code points, in part
    so that bf_tochar() doesn't end up returning an empty string instead of
    generating an error.  (Surrogates are already rejected by put_utf().)

--- utf-ctype.c
+++ utf-ctype.c
@@ -126,7 +126,8 @@ int my_is_printable(int x)
 
     if (x == 0x09)
         return 1;
-    if ((x <= 0xff) && (((x & 0x60) == 0x00) || x == 0x7f))
+    if ((x <= 0xff && ((x & 0x60) == 0x00 || x == 0x7f)) ||
+	(x >= 0xd800 && x <= 0xdfff))
         return 0;
 
     ucd = unicode_character_data(x);
