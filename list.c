f841f3a848421ac07c2ce49beea247f3f4061a32
Roger Crew <crew@cs.stanford.edu>
2010-04-04 04:34:57 -0700

    Fix compiler warning: ans.type uninitialized in do_match

--- list.c
+++ list.c
@@ -746,6 +746,9 @@ do_match(Var arglist, int reverse)
 	ans.v.err = E_INVARG;
     } else
 	switch (match_pattern(pat, subject, regs, reverse)) {
+	default:
+	    panic("do_match:  match_pattern returned unfortunate value.\n");
+	    /*notreached*/
 	case MATCH_SUCCEEDED:
 	    ans = new_list(4);
 	    ans.v.list[1].type = TYPE_INT;
