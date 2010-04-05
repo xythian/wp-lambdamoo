8905095f4975e197940de49c47247c5ddc951a0e
Roger Crew <crew@cs.stanford.edu>
2010-04-05 06:13:09 -0700

    Implement max_string_concat: bf_toliteral

--- list.c
+++ list.c
@@ -713,15 +713,24 @@ bf_tostr(Var arglist, Byte next, void *vdata, Objid progr)
 static package
 bf_toliteral(Var arglist, Byte next, void *vdata, Objid progr)
 {
+    package p;
     Stream *s = new_stream(100);
-    Var r;
 
-    unparse_value(s, arglist.v.list[1]);
-    r.type = TYPE_STR;
-    r.v.str = str_dup(stream_contents(s));
+    TRY_STREAM {
+	Var r;
+
+	unparse_value(s, arglist.v.list[1]);
+	r.type = TYPE_STR;
+	r.v.str = str_dup(stream_contents(s));
+	p = make_var_pack(r);
+    }
+    EXCEPT (stream_too_big) {
+	p = make_space_pack();
+    }
+    ENDTRY_STREAM;
     free_stream(s);
     free_var(arglist);
-    return make_var_pack(r);
+    return p;
 }
 
 struct pat_cache_entry {
