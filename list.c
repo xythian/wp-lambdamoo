2b90993289fbb3b17bc67a23e2cd59649643a1cb
Roger Crew <crew@cs.stanford.edu>
2010-04-05 06:08:47 -0700

    Implement max_string_concat: bf_tostr

--- list.c
+++ list.c
@@ -687,17 +687,27 @@ bf_rindex(Var arglist, Byte next, void *vdata, Objid progr)
 static package
 bf_tostr(Var arglist, Byte next, void *vdata, Objid progr)
 {
+    package p;
     Stream *s = new_stream(100);
-    Var r;
-    int i;
-    for (i = 1; i <= arglist.v.list[0].v.num; i++) {
-	stream_add_tostr(s, arglist.v.list[i]);
+
+    TRY_STREAM {
+	Var r;
+	int i;
+
+	for (i = 1; i <= arglist.v.list[0].v.num; i++) {
+	    stream_add_tostr(s, arglist.v.list[i]);
+	}
+	r.type = TYPE_STR;
+	r.v.str = str_dup(stream_contents(s));
+	p = make_var_pack(r);
     }
-    r.type = TYPE_STR;
-    r.v.str = str_dup(stream_contents(s));
+    EXCEPT (stream_too_big) {
+	p = make_space_pack();
+    }
+    ENDTRY_STREAM;
     free_stream(s);
     free_var(arglist);
-    return make_var_pack(r);
+    return p;
 }
 
 static package
