scm-repo-mirror/wrog_lambdamoo

Add bitwise operators
Add in bitwise AND ".&.", OR ".|.", and XOR ".^."

Add arithmetic left shift "<<", arithmetic right shift ">>",
and logical right shift operators ">>>"

This retains the original parsing of something like "9.^.5" to be
interpreted as "square root of 9"  However, "9.^. 5" is interpreted as
"9 XOR 5".

( The number parsing code moving to parse_number() is the only
  significant change in adapting this extension to the new server;
  the original behavior of the extension is retained in this commit
    --wrog )

Change-Id: I99291bed3d07d68ced204ea27bf684dc09a861fc

code_gen.c
@@ -632,6 +632,10 @@ generate_expr(Expr * expr, State * state)
 	generate_expr(expr->e.expr, state);
 	emit_byte(expr->kind == EXPR_NOT ? OP_NOT : OP_UNARY_MINUS, state);
 	break;
+    case EXPR_COMPLEMENT:
+	generate_expr(expr->e.expr, state);
+	emit_extended_byte(EOP_COMPLEMENT, state);
+	break;
     case EXPR_EQ:
     case EXPR_NE:
     case EXPR_GE:
@@ -694,6 +698,43 @@ generate_expr(Expr * expr, State * state)
 		panic("Not a binary operator in GENERATE_EXPR()");
 	    }
 	    emit_byte(op, state);
+	    pop_stack(1, state);
+	}
+	break;
+    case EXPR_BITAND:
+    case EXPR_BITXOR:
+    case EXPR_BITOR:
+    case EXPR_SHL:
+    case EXPR_SHR:
+    case EXPR_LSHR:
+	{
+	    Extended_Opcode op = EOP_BITAND;	/* initialize to silence warning */
+
+	    generate_expr(expr->e.bin.lhs, state);
+	    generate_expr(expr->e.bin.rhs, state);
+	    switch (expr->kind) {
+	    case EXPR_BITAND:
+		op = EOP_BITAND;
+		break;
+	    case EXPR_BITXOR:
+		op = EOP_BITXOR;
+		break;
+	    case EXPR_BITOR:
+		op = EOP_BITOR;
+		break;
+	    case EXPR_SHL:
+		op = EOP_SHL;
+		break;
+	    case EXPR_SHR:
+		op = EOP_SHR;
+		break;
+	    case EXPR_LSHR:
+		op = EOP_LSHR;
+		break;
+	    default:
+		panic("Not a binary operator in GENERATE_EXPR()");
+	    }
+	    emit_extended_byte(op, state);
 	    pop_stack(1, state);
 	}
 	break;

xythian/wp-lambdamoo

code_gen.c
@@ -626,6 +626,10 @@ generate_expr(Expr * expr, State * state)
 	generate_expr(expr->e.expr, state);
 	emit_byte(expr->kind == EXPR_NOT ? OP_NOT : OP_UNARY_MINUS, state);
 	break;
+    case EXPR_COMPLEMENT:
+	generate_expr(expr->e.expr, state);
+	emit_extended_byte(EOP_COMPLEMENT, state);
+	break;
     case EXPR_EQ:
     case EXPR_NE:
     case EXPR_GE:
@@ -688,6 +692,43 @@ generate_expr(Expr * expr, State * state)
 		panic("Not a binary operator in GENERATE_EXPR()");
 	    }
 	    emit_byte(op, state);
+	    pop_stack(1, state);
+	}
+	break;
+    case EXPR_BITAND:
+    case EXPR_BITXOR:
+    case EXPR_BITOR:
+    case EXPR_SHL:
+    case EXPR_SHR:
+    case EXPR_LSHR:
+	{
+	    Opcode op = EOP_BITAND;	/* initialize to silence warning */
+
+	    generate_expr(expr->e.bin.lhs, state);
+	    generate_expr(expr->e.bin.rhs, state);
+	    switch (expr->kind) {
+	    case EXPR_BITAND:
+		op = EOP_BITAND;
+		break;
+	    case EXPR_BITXOR:
+		op = EOP_BITXOR;
+		break;
+	    case EXPR_BITOR:
+		op = EOP_BITOR;
+		break;
+	    case EXPR_SHL:
+		op = EOP_SHL;
+		break;
+	    case EXPR_SHR:
+		op = EOP_SHR;
+		break;
+	    case EXPR_LSHR:
+		op = EOP_LSHR;
+		break;
+	    default:
+		panic("Not a binary operator in GENERATE_EXPR()");
+	    }
+	    emit_extended_byte(op, state);
 	    pop_stack(1, state);
 	}
 	break;